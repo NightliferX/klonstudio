783cbe5a36a63ee3fce08ee69bb3942f6f5962a8
NightliferX <kohlbrei@gmail.com>
2026-04-13 20:44:36 +0200

    Make dashboard page dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { getSessionStore } from "@/lib/session-store";
 import { getJobs } from "@/lib/jobs";
 import Dashboard from "@/components/dashboard/client-dashboard";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const session = await getSessionStore();
   const initialJobs = await getJobs();
